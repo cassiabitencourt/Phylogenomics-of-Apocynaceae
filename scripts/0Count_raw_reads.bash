f671d95334753659ec39187331ccc860abb48dfc
Cássia Bitencourt <45171352+cassiabitencourt@users.noreply.github.com>
2025-07-25 19:08:25 +0100

    Delete scripts directory

--- scripts/0Count_raw_reads.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --partition=long
-
-for f in *_R*.fastq.gz; do
-   cat "$f" | awk -v fn="$f" -v OFS='\t' 'END{print fn, int(NR/4)}'
-done > raw_reads_summary_after_rename.txt
--- scripts/7FNAtoFASTA.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --partition=long
-
-for file in *.FNA; do
-    mv "$file" "${file%.FNA}.fasta"
-done
