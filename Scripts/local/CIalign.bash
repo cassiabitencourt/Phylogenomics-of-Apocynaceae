1b6feaaa4e8da760bf3f7f0cac1a655e122021db
Cássia Bitencourt <45171352+cassiabitencourt@users.noreply.github.com>
2026-05-07 16:46:57 +0100

    Add CIalign.bash script for processing alignments
    This script processes alignment files using CIAlign and saves the output in a specified directory.

---
+++ Scripts/local/CIalign.bash
@@ -0,0 +1,20 @@
+#!/bin/bash       
+
+# paths
+input_dir="/Users/c.bitencourt/Downloads/alignments_TAXON/3optrimal"
+output_dir="/Users/c.bitencourt/Downloads/alignments_TAXON/4CIalign"
+#outgroup_file="/Users/c.bitencourt/Downloads/alignments_TAXON/outgroups.txt"
+
+# create output
+mkdir -p "$output_dir"
+
+# loop through each alignment
+for file in "$input_dir"/*.aligned.fasta.fasta; do
+	# names
+ 	name=$(basename "$file" .CI.fasta)
+ 	
+ 	echo "Processing: $name"
+
+# Run CIAlign
+CIAlign --infile "$file" --outfile_stem "$output_dir/$name" --crop_ends --remove_divergent --remove_divergent_minperc 0.70 --retain Rubiaceae_Colletoecemateae_Rubioideae_Colletoecema_tortistilum_ERR_5033630 --plot_output --plot_markup
+done
