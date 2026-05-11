776ea15eadafe39f8fbf168a9be8e297ebebc038
Cássia Bitencourt <45171352+cassiabitencourt@users.noreply.github.com>
2026-05-11 17:34:46 +0100

    Update paths in ebg.sh for portability

--- Scripts/local/ebg.sh
+++ Scripts/local/ebg.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # paths
-input_dir="/Users/c.bitencourt/Downloads/alignments_TAXON/7rename_fasta_entries"
-output_dir="/Users/c.bitencourt/Downloads/trees_TAXON/RAxML-NG_AICc"
-RAxMLNG_EXEC=/Users/c.bitencourt/raxml-ng_v2-3/raxml-ng           
-EBG_EXEC=/Users/c.bitencourt/miniforge3/bin/ebg                          
+input_dir="/path/to/alignments/7rename_fasta_entries"
+output_dir="/path/to/trees/RAxML-NG_AICc"
+RAxMLNG_EXEC=/path/to/raxml-ng_v2-3/raxml-ng           
+EBG_EXEC=/path/to/miniforge3/bin/ebg                          
 
 mkdir -p "$output_dir"
 
@@ -20,6 +20,6 @@ for file in "$input_dir"/*.aligned.fasta.tp.fasta.renamed.fa; do
               -tree "$output_dir/$name.raxml.bestTree" \
               -model "$output_dir/$name.raxml.bestModel" \
               -t b \
-              -o "/Users/c.bitencourt/Downloads/trees_TAXON/EBG_AICc/$name.ebg" \
+              -o "/path/to/trees/EBG/$name.ebg" \
               -raxmlng "$RAxMLNG_EXEC"
 done
