35fa053f6f9fa2746adcfcf694233ba37800f31c
Cássia Bitencourt <45171352+cassiabitencourt@users.noreply.github.com>
2026-05-11 17:39:00 +0100

    Update and rename 14wastral_full_collbearing_m4.bash to wastral_collbearing_m4.sh

--- Scripts/local/wastral_collbearing_m4.sh
+++ Scripts/local/wastral_collbearing_m4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash                                  
 
-base="/Users/c.bitencourt/Downloads/trees_TAXON/EBG_collapsed_AICc"
+base="/path/to/trees/EBG_collapsed"
 name="$base/Apocynaceae.bearing.genes.txt"
 trees="$base/gene-trees_Apocynaceae_collbearing_AICc_RAxMLebg.trees"
 
@@ -11,7 +11,7 @@ while read -r line; do
   cat "${base}/${line}.ebg_median_support_prediction.newick" >> "$trees"
 done < $name
 
-WASTRAL_EXEC="/Users/c.bitencourt/ASTER-MacOS/bin/wastral"
+WASTRAL_EXEC="/path/to/ASTER-MacOS/bin/wastral"
 
 $WASTRAL_EXEC --mode 4 -R --support 3 \
 -o "$base/Apocynaceae.wastral.full.annotated.collbearing.AICc.m4.tre" \
