917ccac7d00ba0697d919995d648fa7215260549
Cássia Bitencourt <45171352+cassiabitencourt@users.noreply.github.com>
2026-06-01 16:58:31 +0100

    Create 12.getting.gene.scores.sh

---
+++ Scripts/slurms/12.getting.gene.scores.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#SBATCH --chdir=./
+#SBATCH --job-name=signal-genes-id
+#SBATCH --partition=long
+#SBATCH --mail-user=email@kew.org
+#SBATCH --mail-type=END,FAIL
+
+WARNING="Predicted difficulty:"
+WARNING2="This dataset is considered hard-to-analyze"
+
+> All.noise.gene.scores.tsv
+> All.bearing.gene.scores.tsv
+
+ls *.raxml.log | while read f; do
+    	# signal-based stats
+		score=$(grep "$WARNING" "$f" | awk '{print $NF}')
+		
+		if grep -q "$WARNING2" "$f"; then
+		echo -e "$f\t$score" >> All.noise.gene.scores.tsv
+	else 
+		echo -e "$f\t$score" >> All.bearing.gene.scores.tsv
+	fi
+done
+# >> in case it is not necessary to replace the file when including new metrics
+
+# We used the 'average formula' in excel to obtain the global average score.
